ec46467364c1f53ba620037583de164fbefce18b
Mathieu Robichaud <admin@birkoss.com>
2021-04-06 18:51:05 -0400

    UI fixes for the login and register screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -63,109 +63,113 @@ class _LoginScreenState extends State<LoginScreen> {
     final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Container(
-        child: SafeArea(
-          top: false,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                width: width,
-                height: height * 0.30,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          child: SafeArea(
+            top: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: width,
+                  height: height * 0.30,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 70,
+                      ),
+                      const SizedBox(width: 20),
+                      Text(
+                        'Walleteur',
+                        style: Theme.of(context).textTheme.headline1.copyWith(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .headline1
+                                .color),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/logo.png',
-                      width: 70,
-                    ),
-                    const SizedBox(width: 20),
-                    Text(
-                      'Walleteur',
-                      style: Theme.of(context).textTheme.headline1.copyWith(
-                          color: Theme.of(context)
-                              .primaryTextTheme
-                              .headline1
-                              .color),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          child: Text(
-                            'Login',
-                            style: Theme.of(context).textTheme.headline2,
-                            textAlign: TextAlign.left,
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            child: Text(
+                              'Login',
+                              style: Theme.of(context).textTheme.headline2,
+                              textAlign: TextAlign.left,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          decoration: const InputDecoration(labelText: 'Email'),
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) {
-                            if (value.isEmpty || !value.contains('@')) {
-                              return 'Invalid Email!';
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) =>
-                              _formValues['email'] = newValue,
-                        ),
-                        TextFormField(
-                          decoration:
-                              const InputDecoration(labelText: 'Password'),
-                          obscureText: true,
-                          validator: (value) {
-                            if (value.isEmpty || value.length < 8) {
-                              return 'Invalid Password! Must be at least 8 characters!';
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) =>
-                              _formValues['password'] = newValue,
-                        ),
-                        const SizedBox(height: 20),
-                        ElevatedButton(
-                          child: const Text('LOGIN'),
-                          onPressed: _formSubmitted,
-                        ),
-                        TextButton(
-                          child: const Text('Forgot your password?'),
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushReplacementNamed(RegisterScreen.routeName);
-                          },
-                        ),
-                      ],
+                          SizedBox(height: 10),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value.isEmpty || !value.contains('@')) {
+                                return 'Invalid Email!';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) =>
+                                _formValues['email'] = newValue,
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value.isEmpty || value.length < 8) {
+                                return 'Invalid Password! Must be at least 8 characters!';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) =>
+                                _formValues['password'] = newValue,
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            child: const Text('LOGIN'),
+                            onPressed: _formSubmitted,
+                          ),
+                          TextButton(
+                            child: const Text('Forgot your password?'),
+                            onPressed: () {
+                              Navigator.of(context).pushReplacementNamed(
+                                  RegisterScreen.routeName);
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                child: TextButton(
-                  child: const Text('Don\'t have an account? Register'),
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(fontWeight: FontWeight.bold),
+                Container(
+                  child: TextButton(
+                    child: const Text('Don\'t have an account? Register'),
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(RegisterScreen.routeName);
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .pushReplacementNamed(RegisterScreen.routeName);
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
