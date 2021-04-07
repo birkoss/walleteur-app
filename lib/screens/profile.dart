4c9ef4023be2cca79b2945cc10618a1cdb9396b3
Mathieu Robichaud <admin@birkoss.com>
2021-04-07 16:01:08 -0400

    Fixed an issue with the profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,6 +3,7 @@ import 'package:app/models/person.dart';
 import 'package:app/models/transaction.dart';
 import 'package:app/providers/persons.dart';
 import 'package:app/providers/user.dart';
+import 'package:app/screens/edit_transaction.dart';
 import 'package:app/widgets/empty.dart';
 import 'package:app/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
@@ -18,9 +19,25 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   List<Transaction> transactions;
 
-  Future<void> getTransaction(BuildContext context, String personId) async {
+  String _personId;
+  Person _currentPerson;
+
+  @override
+  void didChangeDependencies() {
+    if (_currentPerson == null) {
+      _personId = ModalRoute.of(context).settings.arguments as String;
+
+      _currentPerson = Provider.of<PersonsProvider>(
+        context,
+        listen: false,
+      ).persons.firstWhere((p) => p.id == _personId);
+    }
+    super.didChangeDependencies();
+  }
+
+  Future<void> _getTransaction() async {
     final response = await Api.get(
-        endpoint: '/v1/person/$personId/transactions',
+        endpoint: '/v1/person/$_personId/transactions',
         token: Provider.of<UserProvider>(context, listen: false).token);
 
     final transactionsData = response['transactions'] as List;
@@ -40,26 +57,23 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final personId = ModalRoute.of(context).settings.arguments as String;
-    Person currentPerson = Provider.of<PersonsProvider>(
-      context,
-      listen: false,
-    ).persons.firstWhere((p) => p.id == personId);
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(currentPerson.name),
+        title: Text(_currentPerson.name),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              // ...
+              Navigator.of(context).pushNamed(
+                EditTransactionScreen.routeName,
+                arguments: _currentPerson.id,
+              );
             },
           )
         ],
       ),
       body: FutureBuilder(
-        future: getTransaction(context, currentPerson.id),
+        future: _getTransaction(),
         builder: (ctx, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
